fdc00767330ab48c09f0ba306cedda5414eaf82f
Francisco Santos <1111315@isep.ipp.pt>
2015-10-14 16:28:42 +0100

    Adding Laravel Models (the main) View and Controllers.

---
+++ app/Commands/CalculatePoints.php
@@ -0,0 +1,39 @@
+<?php namespace App\Commands;
+
+use App\Commands\Command;
+
+use App\Ticket;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Contracts\Queue\ShouldBeQueued;
+
+class CalculatePoints extends Command implements SelfHandling, ShouldBeQueued {
+
+	use InteractsWithQueue, SerializesModels;
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
+		Ticket::chunk(500, function($results) {
+			foreach($results as $result) {
+				$result->updateTicketPoints($result);
+			}
+		});
+	}
+
+}
