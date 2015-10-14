b20bc95e83ea5863b7bdebda7723f2613611d8a4
Francisco Santos <1111315@isep.ipp.pt>
2015-10-14 17:06:45 +0100

    removed backend buttons

--- resources/views/home/landingPage.blade.php
+++ resources/views/home/landingPage.blade.php
@@ -58,7 +58,8 @@
             <ul class="nav navbar-nav">
                 <!--<li class="disabled"><a href="#"><i class="glyphicon glyphicon-user"></i> My stats</a></li>-->
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Backend access <span class="caret"></span></a>
+                    <!--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                        aria-expanded="false">Backend access <span class="caret"></span></a>-->
                     <ul class="dropdown-menu">
                         <li><a href="/tickets"><i class="glyphicon glyphicon-th-list"></i> all tickets</a></li>
                         <li role="separator" class="divider"></li>
